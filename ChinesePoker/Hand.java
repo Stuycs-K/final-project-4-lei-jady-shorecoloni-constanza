d0d5e2efb1e6d5714ce6c721c5792dddb28cedb6
jlei50 <jlei50@stuy.edu>
2024-05-23 11:06:09 -0400

    trying to fix up Hand constructors and basic methods

--- ChinesePoker/Hand.java
+++ ChinesePoker/Hand.java
@@ -8,11 +8,21 @@ public class Hand{
     hand = new ArrayList<Card>();
   }
 
-  public void addCards(Card card){
-    hand.add(card);
+  private boolean addCard(Card card){
+    return hand.add(card);
+  }
+  public boolean addCards(ArrayList<Card> cards){
+    boolean successful = true;
+    for (Card card : cards) {
+      successful = successful && addCard(card);
+    }
+    return successful;
   }
   public Card getCard(int index){
     return hand.get(index);
+  }
+  private Card removeCard(int index){
+    return hand.remove(index);
   }
   public ArrayList<Card> removeCards(ArrayList<Integer> indexes){
     ArrayList<Card> removed = new ArrayList<Card>(indexes.size());
