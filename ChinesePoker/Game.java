c4296216a9e4620e2a4d5e7c06653fb2520fdad2
Constanza Shore Coloni <cshorecoloni50@stuy.edu>
2024-05-24 11:01:40 -0400

    Started creating Game class

---
+++ ChinesePoker/Game.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Game{
+  ArrayList<Player> players;
+  Hand deck;
+
+  public Game(ArrayList<Player> players){
+    this.players = players;
+    deck = new Hand();
+    for(int i = 1; i <= 13; i++){
+      //int strength = i;
+
+      //deck.addCard(new Card())
+    }
+  }
+}
