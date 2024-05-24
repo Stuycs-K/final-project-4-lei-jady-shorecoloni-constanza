02a2e5b286b73dc85c2540eac447e30b488f62cf
Constanza Shore Coloni <cshorecoloni50@stuy.edu>
2024-05-24 00:29:47 -0400

    completed removeCards() and tested Hand class methods

--- ChinesePoker/Hand.java
+++ ChinesePoker/Hand.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Hand{
-  ArrayList<Card> hand;
+  private ArrayList<Card> hand;
 
 
   public Hand(){
@@ -26,9 +26,47 @@ public class Hand{
   }
   public ArrayList<Card> removeCards(ArrayList<Integer> indexes){
     ArrayList<Card> removed = new ArrayList<Card>(indexes.size());
-    for(int i = indexes.size() - 1; i >= 0; i++){
-      removed.add(removed.size(), hand.remove(indexes.get(i)));
+    for(int i = indexes.size() - 1; i >= 0; i--){
+			int index = indexes.get(i);
+			Card remove = hand.get(index);
+      removed.add(remove);
     }
+		for(int i = 0; i < removed.size(); i++){
+			hand.remove(removed.get(i));
+		}
+		return removed;
   }
+	
+	public String toString(){
+		String result = "[";
+		for(int i = 0; i < hand.size(); i++){
+			result += (hand.get(i)).getStrength();
+			if(i < hand.size() -1)
+				result += ", ";
+		}
+		return result +"]";
+	}
+	
+	
+	public static void main(String[] args){
+		Hand test = new Hand();
+		test.addCard(new Card("", 1, ""));
+		ArrayList<Card> toAdd = new ArrayList<Card>();
+		toAdd.add(new Card("", 1, ""));
+		toAdd.add(new Card("", 2, ""));
+		test.addCards(toAdd);
+		System.out.println(test.toString());
+		
+		
+		ArrayList<Integer> indexes = new ArrayList<Integer>();
+		indexes.add(2);
+		indexes.add(1);
+		indexes.add(0);
+		
+		test.removeCards(indexes);
+
+		System.out.println(test.toString());
+		
+	}
 
 }
