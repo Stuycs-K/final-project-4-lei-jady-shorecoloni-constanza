ad96e5ae5b297f7de5837b93dc1099da115d91ec
Constanza Shore Coloni <cshorecoloni50@stuy.edu>
2024-05-24 10:49:14 -0400

    Created Player class, added accessor methods

---
+++ ChinesePoker/Player.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Player{
+  String name;
+  Hand deck;
+  Hand selectedHHand;
+
+  public Player(String name, Hand deck){
+    this.name = name;
+    this.deck = deck;
+    selectedHand = new Hand();
+  }
+
+  public String getName(){
+    return name;
+  }
+
+  public Hand getDeck(){
+    return deck;
+  }
+
+  public Hand getSelectedHand(){
+    return selectedHand;
+  }
+
+}
